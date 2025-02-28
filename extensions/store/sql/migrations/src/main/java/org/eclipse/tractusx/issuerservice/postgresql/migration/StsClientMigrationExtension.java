d413f39927244d8f91f7e62d97cce892ada01d6e
Paul Latzelsperger <paul.latzelsperger@beardyinc.com>
2025-02-28 08:58:57 +0100

    add migrations

---
+++ extensions/store/sql/migrations/src/main/java/org/eclipse/tractusx/issuerservice/postgresql/migration/StsClientMigrationExtension.java
@@ -0,0 +1,43 @@
+/*
+ *   Copyright (c) 2025 Cofinity-X
+ *
+ *   See the NOTICE file(s) distributed with this work for additional
+ *   information regarding copyright ownership.
+ *
+ *   This program and the accompanying materials are made available under the
+ *   terms of the Apache License, Version 2.0 which is available at
+ *   https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ *   WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ *   License for the specific language governing permissions and limitations
+ *   under the License.
+ *
+ *   SPDX-License-Identifier: Apache-2.0
+ *
+ */
+
+package org.eclipse.tractusx.issuerservice.postgresql.migration;
+
+import org.eclipse.edc.runtime.metamodel.annotation.Extension;
+import org.eclipse.edc.runtime.metamodel.annotation.Inject;
+import org.eclipse.edc.spi.security.Vault;
+
+@Extension("STS Client Migration Extension")
+public class StsClientMigrationExtension extends AbstractPostgresqlMigrationExtension {
+    private static final String NAME_SUBSYSTEM = "stsclient";
+
+    @Inject
+    private Vault vault;
+
+    @Override
+    protected Vault getVault() {
+        return vault;
+    }
+
+    @Override
+    protected String getSubsystemName() {
+        return NAME_SUBSYSTEM;
+    }
+}
